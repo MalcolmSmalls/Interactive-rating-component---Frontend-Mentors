98173deb96ec049868448c9e2b26409401735e0b
Malcolm Smalls <smallsbeats@gmail.com>
2022-08-20 12:45:10 -0700

    Fixed javaScript to allow one selection for rating

--- js/main.js
+++ js/main.js
@@ -7,9 +7,81 @@
 
 let value = 0
 
-document.getElementById('oneStar').onclick = oneStar
 
-function oneStar(){
-    document.querySelector('.oneStar').classList.toggle('activated')
-    value = document.querySelector('.oneStar').value
+
+// document.getElementById('oneStar').onclick = oneStar
+// document.getElementById('twoStars').onclick = twoStars
+// document.getElementById('threeStars').onclick = threeStars
+// document.getElementById('fourStars').onclick = fourStars
+// document.getElementById('fiveStars').onclick = fiveStars
+
+
+// let one = document.querySelector('.oneStar')
+// let two = document.querySelector('.twoStars')
+// let three = document.querySelector('.threeStars')
+// let four = document.querySelector('.fourStars')
+// let five = document.querySelector('.fiveStars')
+
+// function oneStar(){
+//     if(twoActivated === true){
+//         two.classList.remove('activated')
+//     }else if(threeActivated === true){
+//         three.classList.remove('activated')
+//     }else if(fourActivated === true ){
+//         four.classList.remove('activated')
+//     }else if(fiveActivated === true){
+//         five.classList.remove('activated')
+//     }
+//     document.querySelector('.oneStar').classList.toggle('activated')
+//     value = document.querySelector('.oneStar').value
+// }
+
+// function twoStars(){
+//     document.querySelector('.twoStars').classList.toggle('activated')
+//     value = document.querySelector('.twoStars').value
+// }
+
+// function threeStars(){
+//     document.querySelector('.threeStars').classList.add('activated')
+//     value = document.querySelector('.threeStars').value
+// }
+
+// function fourStars(){
+//     document.querySelector('.fourStars').classList.toggle('activated')
+//     value = document.querySelector('.fourStars').value
+// }
+
+// function fiveStars(){
+//     document.querySelector('.fiveStars').classList.toggle('activated')
+//     value = document.querySelector('.fiveStars').value
+// }
+
+const ratings = Array.from(document.querySelectorAll('.rating'))
+ratings.forEach(rating => rating.addEventListener('click', activation))
+
+function activation(e){
+    ratings.forEach(element => {
+        if(element !== this){
+            element.classList.remove('activated')
+        }
+    })
+    this.classList.toggle('activated')
+    value = this.value
+    console.log(e.path[0])
 }
+
+// function removeActivation(clicked){
+//     this.classList.toggle('activated')
+//     console.log(this)
+    // if(clicked.target.classList.contains('activated')){
+
+    // }
+    // if (e.classList !== 'activated') return
+    // e.target.classList.toggle('activated')
+//}
+
+let oneActivated = document.querySelector('.oneStar').classList.contains('activated')
+let twoActivated = document.querySelector('.twoStars').classList.contains('activated')
+let threeActivated = document.querySelector('.threeStars').classList.contains('activated')
+let fourActivated = document.querySelector('.fourStars').classList.contains('activated')
+let fiveActivated = document.querySelector('.fiveStars').classList.contains('activated')
