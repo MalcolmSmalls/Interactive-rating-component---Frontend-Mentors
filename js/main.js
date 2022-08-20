097ea8991531fb78b84cb7fbb0579725775d2628
Malcolm Smalls <smallsbeats@gmail.com>
2022-08-20 10:55:45 -0700

    Adding javascript, rating buttons

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
+// document.querySelectorAll('.rating').addEventListener('click',active)
 
+// function active(){
+//     document.querySelector('.rating').classList.toggle('activated')
+//     console.log('hey')
+// }
+
+let value = 0
+
+document.getElementById('oneStar').onclick = oneStar
+
+function oneStar(){
+    document.querySelector('.oneStar').classList.toggle('activated')
+    value = document.querySelector('.oneStar').value
+}
